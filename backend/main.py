39b3b23612090a3828b6701cde76dfbbfce5d2c8
yutaro matsuyama <yuyu@yutaronoMacBook-Air-8.local>
2025-08-03 12:43:54 +0900

    add selected-chips

--- backend/main.py
+++ backend/main.py
@@ -132,4 +132,4 @@ async def get_categories():
     return {"categories": categories}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8001) 
+    uvicorn.run(app, host="0.0.0.0", port=8001)
